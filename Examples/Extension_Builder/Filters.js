0b80c2701b3fbd950ccdea2feeec3e33b312bc62
Obvious Alex C <76855369+David-Orangemoon@users.noreply.github.com>
2023-06-14 13:27:18 -0400

    Update Filters.js

--- Examples/Extension_Builder/Filters.js
+++ Examples/Extension_Builder/Filters.js
@@ -1,22 +1,21 @@
 (function (Scratch) {
-    class ExtensionBuilder{constructor(n,t,e,i){Scratch=Scratch||{TargetType:{SPRITE:"sprite",STAGE:"stage"},BlockType:{COMMAND:"command",REPORTER:"reporter",BOOLEAN:"Boolean",HAT:"hat"},ArgumentType:{STRING:"string",NUMBER:"number",COLOR:"color",ANGLE:"angle",BOOLEAN:"Boolean",MATRIX:"matrix",NOTE:"note"},vm:window.vm,Cast:{toNumber:n=>Number(n),toString:n=>String(n),toBoolean:n=>Boolean(n)},extensions:{unsandboxed:!0,register(n){let t=vm.extensionManager._registerInternalExtension(n);vm.extensionManager._loadedExtensions.set(n.getInfo().id,t)}}},this.internal={},this.internal.JSON={blocks:[],menus:{}},this.internal.defaultFunction={code(){console.log("This block has no code")},arguments:{}},this.addDocs=n=>{this.internal.JSON.docsURI=n},this.addBlock=(n,t,e,i,o,l)=>{i=i||this.internal.defaultFunction.code,this[t]=i,l=l||{};let r=l;r.disableMonitor||(r.disableMonitor=!0),r.opcode=t,r.blockType=e,r.text=n,r.arguments=o||this.internal.defaultFunction.arguments;let s=this.internal.JSON.blocks.length;return this.internal.JSON.blocks.push(r),this.internal.JSON.blocks[s].addArgument=(n,t,e,i)=>{if(null==(e=e||null))switch(typeof t){case"string":default:e=Scratch.ArgumentType.STRING;break;case"boolean":e=Scratch.ArgumentType.BOOLEAN;break;case"number":case"bigint":e=Scratch.ArgumentType.NUMBER}return this.internal.JSON.blocks[s].arguments[n]={type:e,defaultValue:t},(i=i||null)&&"string"==typeof i&&(this.internal.JSON.blocks[s].arguments[n].menu=i),this.internal.JSON.blocks[s]},this.internal.JSON.blocks[s].setFilter=n=>(n=n||Scratch.TargetType.SPRITE,this.internal.JSON.blocks[s].filter=n,this.internal.JSON.blocks[s]),this.internal.JSON.blocks[s]},this.addMenu=(n,t,e)=>{e=e||!1,"function"==typeof t?(this[n+"Function"]=t,this.internal.JSON.menus[n]={items:n+"Function"}):this.internal.JSON.menus[n]={items:t},this.internal.JSON.menus[n].acceptReporters=e},this.internal.createBase=()=>{if(n=n||"Extension",t=t||"extension",this.internal.JSON.name=n,this.internal.JSON.id=t,(e=e||{}).blockColor=e.blockColor||null,e.inputColor=e.inputColor||null,e.outlineColor=e.outlineColor||null,null!=e.blockColor){let o=e.blockColor;o>8947848?this.internal.colors=[o,o-197379,o-394758,]:this.internal.colors=[o,o+197379,o+394758,],e.inputColor,this.internal.colors[1]=e.inputColor,e.outlineColor,this.internal.colors[2]=e.outlineColor,this.internal.JSON.color1=this.internal.colors[0],this.internal.JSON.color2=this.internal.colors[1],this.internal.JSON.color3=this.internal.colors[2]}(i=i||{}).blockIconUri=i.blockIconUri||null,i.menuIconUri=i.menuIconUri||i.blockIconUri||null,this.menuUri=i.menuIconUri,this.blockIco=i.blockIconUri,this.docsUri=null},this.internal.createBase(),this.getInfo=()=>this.internal.JSON,this.register=()=>{Scratch.extensions.register(this)}}}
-    
-    const extension = new ExtensionBuilder("Test Extension", "tExt")
-  
-    extension.addBlock("This is a boolean that only appears on both!","tBlock1",Scratch.BlockType.BOOLEAN,() => {
+  class ExtensionBuilder{constructor(t,n,i,l){this.internal={},this.internal.JSON={blocks:[],menus:{}},this.runtime=Scratch.vm.runtime,this.internal.defaultFunction={code(){console.log("This block has no code")},arguments:{}},this.addDocs=t=>{this.internal.JSON.docsURI=t},this.addBlock=(t,n,i,l,e,s)=>{l=l||this.internal.defaultFunction.code,this[n]=l,s=s||{};let o=s;o.disableMonitor||(o.disableMonitor=!0),o.opcode=n,o.blockType=i,o.text=t,o.arguments=e||this.internal.defaultFunction.arguments;let r=this.internal.JSON.blocks.length;return this.internal.JSON.blocks.push(o),this.internal.JSON.blocks[r].addArgument=(t,n,i,l)=>{if(null==(i=i||null))switch(typeof n){case"string":default:i=Scratch.ArgumentType.STRING;break;case"boolean":i=Scratch.ArgumentType.BOOLEAN;break;case"number":case"bigint":i=Scratch.ArgumentType.NUMBER}return null==n?this.internal.JSON.blocks[r].arguments[t]={type:i}:this.internal.JSON.blocks[r].arguments[t]={type:i,defaultValue:n},(l=l||null)&&"string"==typeof l&&(this.internal.JSON.blocks[r].arguments[t].menu=l),this.internal.JSON.blocks[r]},this.internal.JSON.blocks[r].setIcon=t=>{this.internal.JSON.blocks[r].blockIconURI=t},this.internal.JSON.blocks[r].setFilter=t=>(t=t||Scratch.TargetType.SPRITE,this.internal.JSON.blocks[r].filter=t,this.internal.JSON.blocks[r]),this.internal.JSON.blocks[r].hideBlock=()=>{this.internal.JSON.blocks[r].hideFromPalette=!0},this.internal.JSON.blocks[r].stopMoniter=()=>{this.internal.JSON.blocks[r].disableMonitor=!0},this.internal.JSON.blocks[r].setEdgeActivation=t=>{this.internal.JSON.blocks[r].isEdgeActivated=t},this.internal.JSON.blocks[r].addImage=(t,n,i)=>{i=i||!1;let l={type:Scratch.ArgumentType.IMAGE,dataURI:n,flipRTL:i};this.internal.JSON.blocks[r].arguments[t]=l},this.internal.JSON.blocks[r]},this.addMenu=(t,n,i)=>{i=i||!1,"function"==typeof n?(this[t+"Function"]=n,this.internal.JSON.menus[t]={items:t+"Function"}):this.internal.JSON.menus[t]={items:n},this.internal.JSON.menus[t].acceptReporters=i},this.addButton=(t,n,i)=>{n=n||this.internal.defaultFunction.code,i=i||"Button",this["button_"+t]=n;let l={};l.func="button_"+t,l.blockType=Scratch.BlockType.BUTTON,l.text=i;let e=this.internal.JSON.blocks.length;return this.internal.JSON.blocks[e]=l,this.internal.JSON.blocks[e]},this.addDivider=()=>{this.internal.JSON.blocks.push("---")},this.addLabel=t=>{t=t||"N/A";let n={opcode:"__NOUSEOPCODE",blockType:"label",text:t};this.internal.JSON.blocks.push(n)},this.__NOUSEOPCODE=()=>{},this.internal.createBase=()=>{if(t=t||"Extension",n=n||"extension",this.internal.JSON.name=t,this.internal.JSON.id=n,(i=i||{}).blockColor=i.blockColor||null,i.inputColor=i.inputColor||null,i.outlineColor=i.outlineColor||null,null!=i.blockColor){let e=i.blockColor;e>8947848?this.internal.colors=[e,e-197379,e-394758,]:this.internal.colors=[e,e+197379,e+394758,],i.inputColor,this.internal.colors[1]=i.inputColor,i.outlineColor,this.internal.colors[2]=i.outlineColor,this.internal.JSON.color1=this.internal.colors[0],this.internal.JSON.color2=this.internal.colors[1],this.internal.JSON.color3=this.internal.colors[2]}(l=l||{}).blockIconUri=l.blockIconUri||null,l.menuIconUri=l.menuIconUri||l.blockIconUri||null,this.menuUri=l.menuIconUri,this.blockIco=l.blockIconUri,this.docsUri=null},this.internal.createBase(),this.setColors=(t,n,i)=>{t="string"==typeof t?t:(t+0).toString(16),n="string"==typeof n?n:(n+0).toString(16),i="string"==typeof i?i:(i+0).toString(16),this.internal.colors=[0,0,0],this.internal.colors[0]=t,this.internal.colors[1]=n,this.internal.colors[2]=i,this.internal.JSON.color1=t,this.internal.JSON.color2=n,this.internal.JSON.color3=i},this.setMenuIcon=t=>{this.internal.JSON.menuIconURI=t},this.setGlobalBlockIcon=t=>{this.internal.JSON.blockIconURI=t},this.runHat=t=>{this.runtime.startHats(this.internal.JSON.id+"_"+t)},this.getInfo=()=>this.internal.JSON,this.register=()=>{Scratch.extensions.register(this)}}}
+   
+  const extension = new ExtensionBuilder("Test Extension", "tExt")
+
+  extension.addBlock("This is a boolean that only appears on both!","tBlock1",Scratch.BlockType.BOOLEAN,() => {
+    console.log("This Works!")
+  })
+
+  extension.addBlock("This is a boolean that only appears on the stage!","tBlock2",Scratch.BlockType.BOOLEAN,() => {
+    console.log("This Works!")
+  }).setFilter(Scratch.TargetType.STAGE)
+
+  extension.addBlock("This is a boolean that only appears on the sprite!","tBlock3",Scratch.BlockType.BOOLEAN,() => {
       console.log("This Works!")
-    })
-  
-    extension.addBlock("This is a boolean that only appears on the stage!","tBlock2",Scratch.BlockType.BOOLEAN,() => {
-      console.log("This Works!")
-    }).setFilter(Scratch.TargetType.STAGE)
-  
-    extension.addBlock("This is a boolean that only appears on the sprite!","tBlock3",Scratch.BlockType.BOOLEAN,() => {
-        console.log("This Works!")
-    }).setFilter(Scratch.TargetType.SPRITE)
-  
-    extension.addMenu("Menu",["1","2","3"])
-  
-    extension.register()
-  })(window.Scratch);
-  
+  }).setFilter(Scratch.TargetType.SPRITE)
+
+  extension.addMenu("Menu",["1","2","3"])
+
+  extension.register()
+})(window.Scratch);
